1a56d7c0e881f49f0eaf5944a7f0ca374f48378c
Cordarro Storme Higgins Redman <cordarrohiggins@gmail.com>
2026-05-05 20:39:27 -0400

    small error fix

--- app/games/[gameId]/page.tsx
+++ app/games/[gameId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
+//import Link from "next/link";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import CheckCountdown from "@/components/CheckCountdown";
